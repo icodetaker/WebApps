d28446c6b50d866b2d6752edfcfb5e42dc2f91f8
iman <everybuddy@outlook.com>
2018-05-05 02:36:11 -0700

    Call javascript on server and use data from C# Use chartist to render chart on server >npm install --save node-chartist

--- Server-Prerender/Controller/HomeController.cs
+++ Server-Prerender/Controller/HomeController.cs
@@ -2,7 +2,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.NodeServices;
 
-namespace Prerending.HomeController {
+namespace Prerending.HomeController
+{
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -12,10 +13,32 @@ namespace Prerending.HomeController {
 
         public async Task<IActionResult> Chart([FromServices] INodeServices nodeServices)
         {
-            ViewData["ResultFromNode"] = await nodeServices.InvokeAsync<string>("ChartModule.js");
+            var options = new
+            {
+                width = 400,
+                height = 200,
+                showArea = true,
+                showPoint = true,
+                fullWidth = true
+            };
+
+            var data = new
+            {
+                labels = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" },
+                series = new[] {
+                    new[] { 1, 5, 2, 5, 4, 3},
+                    new[] { 2, 3, 5, 8, 1, 2},
+                    new[] { 5, 4, 3, 2, 1, 0}
+                }
+            };
+
+
+            ViewData["ResultFromNode"] = await nodeServices.InvokeAsync<string>(
+                "ChartModule.js",
+                "line", options, data);
             return View();
         }
 
-        
+
     }
 }
